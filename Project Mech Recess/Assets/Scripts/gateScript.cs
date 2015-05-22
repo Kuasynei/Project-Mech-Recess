6fce390b0d76dc5dbffa3ceb5e2ad7c45a3f04a1
Kuasynei <di.oxide.inc@gmail.com>
2015-05-21 22:51:33 -0400

    Stable Build
    Speed limit code changed, boost increased, gate added (with transparency
    added for visual feedback), camera will now push farther from the wall
    to avoid clipping when a collision is detected. Attack code begun.

---
+++ Project Mech Recess/Assets/Scripts/gateScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gateScript : MonoBehaviour {
+    // Time in seconds since set as un-triggered
+    public float timeSince = 10.0f;
+
+    // Reset value for timeSince
+    private float beginTime;
+
+	// Use this for initialization
+	void Start () 
+    {
+        beginTime = timeSince;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //Debug.Log("Time since exit: " + timeSince);
+        if (GetComponent<Collider>().isTrigger == false)
+        {
+            timeSince -= Time.deltaTime;
+
+            if (timeSince <= 0.0f)
+            {
+                GetComponent<Collider>().isTrigger = true;
+
+                timeSince = 0.0f;
+
+                GetComponentInChildren<MeshRenderer>().material.color = new Vector4(1f, 1f, 1f, 0.5f);
+                
+            }
+        }
+	}
+
+    // Calls setTriggered on exit of trigger
+    void OnTriggerExit(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            setTriggered();
+        }
+    }
+
+    // Sets collider not triggered
+    void setTriggered()
+    {
+        if (GetComponent<Collider>().isTrigger == true)
+        {
+            GetComponent<Collider>().isTrigger = false;
+
+            timeSince = beginTime;
+
+            GetComponentInChildren<MeshRenderer>().material.color = new Vector4(1f, 1f, 1f, 0f);
+        }
+    }
+}
--- Project Mech Recess/Assets/Scripts/mechBehaviour.cs
+++ Project Mech Recess/Assets/Scripts/mechBehaviour.cs
@@ -60,223 +60,223 @@ public class mechBehaviour : MonoBehaviour {
         windSystem.transform.rotation = Quaternion.Euler(-90, 0, 0);
 	}
 
-    void FixedUpdate()
-    {
-        ////Wind FX
-        //Wind that appears below player to help with landing
-        RaycastHit verticalAlignHit;
-        if (Physics.Raycast(transform.position, -transform.up, out verticalAlignHit, 30f))
-        {
-            windSystem.transform.position = verticalAlignHit.point;
-            if (!windSystem.GetComponent<ParticleSystem>().isPlaying)
-            {
-                windSystem.GetComponent<ParticleSystem>().Play();
-            }
-        }
-        else
-            windSystem.GetComponent<ParticleSystem>().Stop();
-
-        //Wind to indicate exceeding regular top speed
-        speedWind.transform.position = transform.position;
-        speedWind.transform.LookAt(RB.velocity + transform.position, transform.up);
-        speedWind.GetComponent<ParticleSystem>().emissionRate = (RB.velocity.magnitude - (topSpeed + 5f)) * 20;
-    }
-
     void Update()
     {
         ////Input
-        hAxesInput = Input.GetAxis("Horizontal");
-        vAxesInput = Input.GetAxis("Vertical");
-        jumpAxes = Input.GetAxis("Jump");
-        fire1Axes = Input.GetAxis("Fire1");
-        fire2Axes = Input.GetAxis("Fire2");
-        shiftTechInput = Input.GetKey(KeyCode.LeftShift);
-
-        ////Cursor Relock
-        if (Cursor.lockState != CursorLockMode.Locked && fire1Axes != 0)
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
-
-        ////Wall Slide
-        //Limiting horizontal movement when in a wall slide.
-        float wallDampener = 1;
-        if (onWall)
-        {
-            wallDampener = 0.6f;
-            if (RB.velocity.y < -3.5f)
-            {
-                RB.velocity = new Vector3(RB.velocity.x, -3.5f, RB.velocity.z);
-            }
-        }
-
-        //Wall jump turn limiter
-        if (wallJumpTurnSpeed < 1)
-            wallJumpTurnSpeed += Time.deltaTime/3;
-        else
-            wallJumpTurnSpeed = 1;
-
-        ////Horizontal Movement
+		hAxesInput = Input.GetAxis("Horizontal");
+		vAxesInput = Input.GetAxis("Vertical");
+		jumpAxes = Input.GetAxis("Jump");
+		fire1Axes = Input.GetAxis("Fire1");
+		fire2Axes = Input.GetAxis("Fire2");
+		shiftTechInput = Input.GetKey(KeyCode.LeftShift);
+		
+		////Cursor Relock
+		if (Cursor.lockState != CursorLockMode.Locked && fire1Axes != 0)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+		
+		////Wall Slide
+		//Limiting horizontal movement when in a wall slide.
+		float wallDampener = 1;
+		if (onWall)
+		{
+			wallDampener = 0.6f;
+			if (RB.velocity.y < -3.5f)
+			{
+				RB.velocity = new Vector3(RB.velocity.x, -3.5f, RB.velocity.z);
+			}
+		}
+		
+		//Wall jump turn limiter
+		if (wallJumpTurnSpeed < 1)
+			wallJumpTurnSpeed += Time.deltaTime/3;
+		else
+			wallJumpTurnSpeed = 1;
+		
+		////Horizontal Movement
 		RB.AddForce(new Vector3(mainCamera.transform.forward.x, 0, mainCamera.transform.forward.z) * (acceleration * Time.deltaTime * 100f)
 		            * vAxesInput * wallDampener * wallJumpTurnSpeed * (boostCooldown_Var + 1));
 		RB.AddForce(new Vector3(mainCamera.transform.right.x, 0, mainCamera.transform.right.z) * (acceleration * Time.deltaTime * 100f)
 		            * hAxesInput * wallDampener * wallJumpTurnSpeed * (boostCooldown_Var + 1));
-
-        ////Rotate Towards Movement
-        //If 
-        if (new Vector3(RB.velocity.normalized.x, 0, RB.velocity.normalized.z) != new Vector3(0,0,0))
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(new Vector3(RB.velocity.normalized.x, 0, RB.velocity.normalized.z)), 
-                                                            Mathf.Clamp(RB.velocity.magnitude, 3f, 20f));
-        }
-
-        ////Speed Limit
-        {
-            Vector3 tempXZ = new Vector3(RB.velocity.x, 0f, RB.velocity.z);
-            Vector3 tempY = new Vector3(0f, RB.velocity.y, 0f);
-
-            //Limit only horizontal speed if the player's boost not being used.
-            if (tempXZ.magnitude > topSpeed && boostCooldown_Var <= boostCooldown - 0.5f)
-            {
-                RB.velocity = tempXZ.normalized * topSpeed + tempY;
-            }
-            else if (boostCooldown_Var > 0)//Hard Limit
-            {
-				RB.velocity = new Vector3(Mathf.Clamp(tempXZ.x, -15, 15), Mathf.Clamp(tempY.y, -12, 12), Mathf.Clamp(tempXZ.z, -15, 15));
-			}
+		
+		////Rotate Towards Movement
+		if (new Vector3(RB.velocity.normalized.x, 0, RB.velocity.normalized.z) != new Vector3(0,0,0))
+		{
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(new Vector3(RB.velocity.normalized.x, 0, RB.velocity.normalized.z)), 
+			                                              Mathf.Clamp(RB.velocity.magnitude, 3f, 20f));
+		}
+		
+		////Speed Limit
+		{
+			Vector3 tempXZ = new Vector3(RB.velocity.x, 0f, RB.velocity.z);
+			Vector3 tempY = new Vector3(0f, RB.velocity.y, 0f);
 			
+			//Limit only horizontal speed if the player's boost not being used.
+			if (tempXZ.magnitude > topSpeed && boostCooldown_Var <= boostCooldown - 0.5f)
+			{
+				RB.velocity = tempXZ.normalized * topSpeed + tempY;
+			}
+			else if (boostCooldown_Var > 0)//Hard Limit
+			{
+                RB.velocity = new Vector3(0, Mathf.Clamp(tempY.y, -12, 15), 0) + Vector3.ClampMagnitude(new Vector3(tempXZ.x, 0, tempXZ.z), 40f);
+			}
+		}
+		
+		////Reticle
+		//Getting the ray from the center of the camera, forwards.
+		Vector3 defaultPoint = mainCamera.transform.position + mainCamera.transform.forward * reticleMaxDistance;
+		
+		Ray cameraRay = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
+		RaycastHit[] cameraRayHit = Physics.RaycastAll(cameraRay, reticleMaxDistance);
+		
+		//Firing a new ray from the player, to the first point of collision from the camera ray.
+		Ray playerRay = new Ray();
+		RaycastHit[] playerRayHit;
+		
+		float nearest = Mathf.Infinity;
+		if (cameraRayHit.Length > 0)
+		{
+			foreach (RaycastHit cameraHitInfo in cameraRayHit)
+			{
+				if (cameraHitInfo.distance < nearest)
+				{
+					nearest = cameraHitInfo.distance;
+					if (cameraHitInfo.collider.tag != "Player")
+						playerRay = new Ray(transform.position, (transform.position - cameraHitInfo.point).normalized * -reticleMaxDistance);
+				}
+			}
+		}
+		else
+			playerRay = new Ray(transform.position, (transform.position - defaultPoint).normalized * -reticleMaxDistance);
+		
+		playerRayHit = Physics.RaycastAll(playerRay, reticleMaxDistance);
+		
+		//If player ray hits terrain move the reticle to the point of collision. If it hits nothing move it to default point.
+		nearest = Mathf.Infinity;
+		if (playerRayHit.Length > 0)
+		{
+			foreach (RaycastHit playerRayHitInfo in playerRayHit)
+			{
+				if (playerRayHitInfo.distance < nearest)
+				{
+					nearest = playerRayHitInfo.distance;
+					if (playerRayHitInfo.collider.tag != "Player")
+						reticleObj.transform.position = playerRayHitInfo.point;
+				}
+			}
+		}
+		else
+			reticleObj.transform.position = defaultPoint;
+		
+		////Jumps
+		//Ground Detection
+		RaycastHit groundHit;
+		if (Physics.Raycast(transform.position, -transform.up, out groundHit, transform.lossyScale.y + 0.3f))
+		{
+			onGround = true;
+			onWall = false;
+			inWallRun = false;
+			wallJumpTurnSpeed = 1;
+		}
+		else
+			onGround = false;
+		
+		//If on the ground, not recovering, and the jump buttons were pressed.
+		if (jumpAxes != 0 && onGround && landRecovery <= 0f)
+			RB.AddForce(0f, jumpPower * Time.deltaTime, 0f, ForceMode.VelocityChange);
+		
+		//Wall Jump is in OnTriggerStay
+		
+		//If not on the ground or on a wall increase landing recovery to 0.2, if on the ground or a wall decrease landing recovery over time.
+		if (!onGround && !onWall)
+			landRecovery = 0.1f;
+		else
+		{
+			landRecovery -= Time.deltaTime;
+			boostNumber_Var = boostNumber; //Available boosts refill to max
+		}
+		
+		////Glide & Fast Fall
+		if (RB.velocity.y < -0.2f && (vAxesInput != 0 || hAxesInput != 0))
+		{
+			RB.AddForce(0f, 20f, 0f);
+		}
+		else if (RB.velocity.y < 0f)
+			RB.AddForce(0f, -25f, 0f);
+		
+		//Perpetual Fast Fall
+		RB.AddForce(0f, -20f, 0f);
+		
+		
+		////Click Boost
+		//If boost is on cooldown
+		if (boostCooldown_Var > 0f)
+			boostCooldown_Var -= Time.deltaTime;
+		
+		if (boostNumber_Var > 0)
+			boostLight.GetComponent<Light>().color = new Color(1f, 0.3f, 0f);
+		
+		if (boostNumber_Var == 0)
+			boostLight.GetComponent<Light>().color = new Color(1f, 0.1f, 0f);
+		
+		//If boost is currently being performed.
+		if (boostCooldown_Var > boostCooldown - 0.5f)
+		{
+			GameObject tempLight = Instantiate(boostLight, transform.position - transform.forward, Quaternion.identity) as GameObject;
+			Destroy(tempLight, 0.2f);
 		}
 
-        ////Reticle
-        //Getting the ray from the center of the camera, forwards.
-        Vector3 defaultPoint = mainCamera.transform.position + mainCamera.transform.forward * reticleMaxDistance;
+        Debug.DrawRay(transform.position, cameraRay.direction * 5f, Color.white);
 
-        Ray cameraRay = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
-        RaycastHit[] cameraRayHit = Physics.RaycastAll(cameraRay, reticleMaxDistance);
-
-        //Firing a new ray from the player, to the first point of collision from the camera ray.
-        Ray playerRay = new Ray();
-        RaycastHit[] playerRayHit;
-
-        float nearest = Mathf.Infinity;
-        if (cameraRayHit.Length > 0)
-        {
-            foreach (RaycastHit cameraHitInfo in cameraRayHit)
-            {
-                if (cameraHitInfo.distance < nearest)
-                {
-                    nearest = cameraHitInfo.distance;
-                    if (cameraHitInfo.collider.tag != "Player")
-                        playerRay = new Ray(transform.position, (transform.position - cameraHitInfo.point).normalized * -reticleMaxDistance);
-                }
-            }
-        }
-        else
-            playerRay = new Ray(transform.position, (transform.position - defaultPoint).normalized * -reticleMaxDistance);
-
-        playerRayHit = Physics.RaycastAll(playerRay, reticleMaxDistance);
-
-        //If player ray hits terrain move the reticle to the point of collision. If it hits nothing move it to default point.
-        nearest = Mathf.Infinity;
-        if (playerRayHit.Length > 0)
-        {
-            foreach (RaycastHit playerRayHitInfo in playerRayHit)
-            {
-                if (playerRayHitInfo.distance < nearest)
-                {
-                    nearest = playerRayHitInfo.distance;
-                    if (playerRayHitInfo.collider.tag != "Player")
-                        reticleObj.transform.position = playerRayHitInfo.point;
-                }
-            }
-        }
-        else
-            reticleObj.transform.position = defaultPoint;
-
-        ////Jumps
-        //Ground Detection
-        RaycastHit groundHit;
-        if (Physics.Raycast(transform.position, -transform.up, out groundHit, transform.lossyScale.y + 0.3f))
-        {
-            onGround = true;
-            onWall = false;
-            inWallRun = false;
-            wallJumpTurnSpeed = 1;
-        }
-        else
-            onGround = false;
-
-        //If on the ground, not recovering, and the jump buttons were pressed.
-        if (jumpAxes != 0 && onGround && landRecovery <= 0f)
-            RB.AddForce(0f, jumpPower * Time.deltaTime, 0f, ForceMode.VelocityChange);
-        
-        //Wall Jump is in OnTriggerStay
-
-        //If not on the ground or on a wall increase landing recovery to 0.2, if on the ground or a wall decrease landing recovery over time.
-        if (!onGround && !onWall)
-            landRecovery = 0.1f;
-        else
-        {
-            landRecovery -= Time.deltaTime;
-            boostNumber_Var = boostNumber; //Available boosts refill to max
-        }
-
-        ////Glide & Fast Fall
-        if (RB.velocity.y < -0.2f && (vAxesInput != 0 || hAxesInput != 0))
-        {
-            RB.AddForce(0f, 20f, 0f);
-        }
-        else if (RB.velocity.y < 0f)
-            RB.AddForce(0f, -25f, 0f);
-
-        //Perpetual Fast Fall
-        RB.AddForce(0f, -20f, 0f);
-
-
-        ////Click Boost
-        //If boost is on cooldown
-        if (boostCooldown_Var > 0f)
-            boostCooldown_Var -= Time.deltaTime;
-
-        if (boostNumber_Var > 0)
-            boostLight.GetComponent<Light>().color = new Color(1f, 0.3f, 0f);
-
-        if (boostNumber_Var == 0)
-            boostLight.GetComponent<Light>().color = new Color(1f, 0.1f, 0f);
-
-        //If boost is currently being performed.
-        if (boostCooldown_Var > boostCooldown - 0.5f)
-        {
-            GameObject tempLight = Instantiate(boostLight, transform.position - transform.forward, Quaternion.identity) as GameObject;
-            Destroy(tempLight, 0.2f);
-        }
-
-        //If player presses fire1 and boost is off cooldown.
-        if (fire1Axes != 0 && boostCooldown_Var <= 0 && boostNumber_Var > 0)
-        {
+		//If player presses fire1 and boost is off cooldown.
+		if (fire1Axes != 0 && boostCooldown_Var <= 0 && boostNumber_Var > 0)
+		{
 			if (onGround)
 				RB.AddForce(cameraRay.direction * boostPower * (wallJumpTurnSpeed), ForceMode.Impulse);
 			else
 				RB.AddForce(cameraRay.direction * boostPower * (wallJumpTurnSpeed) + transform.up * 8f, ForceMode.Impulse);
-            boostCooldown_Var = boostCooldown;
-            boostNumber_Var--;
-        }
+			boostCooldown_Var = boostCooldown;
+			boostNumber_Var--;
+		}
+		
+		////Chaser Attack
+		//If player presses the right mouse button
+		if (fire2Axes != 0)
+		{
+			
+		}
+    }
 
-        ////Chaser Attack
-        //If player presses the right mouse button
-        if (fire2Axes != 0)
-        {
+    void FixedUpdate()
+    {
+		////Wind FX
+		//Wind that appears below player to help with landing
+		RaycastHit verticalAlignHit;
+		if (Physics.Raycast(transform.position, -transform.up, out verticalAlignHit, 30f))
+		{
+			windSystem.transform.position = verticalAlignHit.point;
+			if (!windSystem.GetComponent<ParticleSystem>().isPlaying)
+			{
+				windSystem.GetComponent<ParticleSystem>().Play();
+			}
+		}
+		else
+			windSystem.GetComponent<ParticleSystem>().Stop();
+		
+		//Wind to indicate exceeding regular top speed
+		speedWind.transform.position = transform.position;
+		speedWind.transform.LookAt(RB.velocity + transform.position, transform.up);
+		speedWind.GetComponent<ParticleSystem>().emissionRate = (RB.velocity.magnitude - (topSpeed + 5f)) * 20;
 
-        }
-
-        ////Player Transparency
-        if (Vector3.Distance(transform.position, mainCamera.transform.position) <= 3f && transparency > 0)
-            transparency -= Time.deltaTime * 2;
-        else if (transparency < 1)
-            transparency += Time.deltaTime * 2;
-
-        GetComponent<MeshRenderer>().material.color = new Vector4(1f, 1f, 1f, transparency);
+		////Player Transparency
+		if (Vector3.Distance(transform.position, mainCamera.transform.position) <= 3f && transparency > 0)
+			transparency -= Time.fixedDeltaTime * 2;
+		else if (transparency < 1)
+            transparency += Time.fixedDeltaTime * 2;
+		
+		GetComponent<MeshRenderer>().material.color = new Vector4(1f, 1f, 1f, transparency);
     }
 
     //Handling Wall Collisions and Wall State
